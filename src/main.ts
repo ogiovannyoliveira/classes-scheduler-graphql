ff1904d57ae74b598eaeeedfe7857ded48b200c9
Giovanny Lucas <giolucas@uolinc.com>
2022-07-10 16:51:41 -0300

    refactor: :recycle: updated initialization function

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-async function bootstrap(): Promise<void> {
+(async function bootstrap(): Promise<void> {
   const { APP_PORT } = process.env;
 
   const app = await NestFactory.create(AppModule);
@@ -10,5 +10,4 @@ async function bootstrap(): Promise<void> {
   await app.listen(APP_PORT, () =>
     console.log('🚀 Server is running on port %s! 🧑‍🚀', APP_PORT),
   );
-}
-bootstrap();
+})();
